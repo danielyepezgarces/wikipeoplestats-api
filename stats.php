a9feb305d322563b06e096e97b403163e0124658
Daniel Yepez Garces <danielyepezgarces@gmail.com>
2025-01-23 16:39:58 -0500

    Update stats.php

--- stats.php
+++ stats.php
@@ -1,5 +1,9 @@
 <?php
 header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 
 include 'config.php';
 
