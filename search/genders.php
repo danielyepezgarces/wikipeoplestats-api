ef3e950b85d4a2a2e6981bc26932b26809eb75fe
Daniel Yepez Garces <danielyepezgarces@gmail.com>
2025-01-23 19:36:42 -0500

    Update genders.php

--- search/genders.php
+++ search/genders.php
@@ -1,5 +1,8 @@
 <?php
-header('Content-Type: application/json; charset=utf-8');
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 include '../languages.php';
 
