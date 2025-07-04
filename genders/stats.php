1d9a99a216668fb89cab2a6492eeed3303949ccd
Daniel Yepez Garces <git@danielyepezgarces.com.co>
2025-07-04 14:12:56 -0500

    Refactor stats.php to remove unnecessary CORS headers for improved security

--- genders/stats.php
+++ genders/stats.php
@@ -1,8 +1,6 @@
 <?php
 header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 include '../config.php';
 include '../languages.php';
